2ed49fe7182bc99d135d9ef16b39786de1f24d23
Dano <leesy@doxmeet.com>
2026-02-05 20:45:18 +0900

    feat: add TopNav header to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import { Geist } from "next/font/google"
 
 import "./globals.css"
 
+// 우리가 수정한 헤더 컴포넌트
+import { TopNav } from "@/header/top-nav"
+
 const geist = Geist({
   subsets: ["latin"],
   variable: "--font-geist",
@@ -28,7 +31,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko" suppressHydrationWarning>
-      <body className={`${geist.variable} font-sans antialiased`}>{children}</body>
+      <body className={`${geist.variable} font-sans antialiased`}>
+        <div className="min-h-dvh flex flex-col">
+          {/* 상단 헤더 영역 */}
+          <TopNav
+            searchQuery=""
+            onSearchChange={() => {}}
+            selectedSpecialty="all"
+            onSpecialtyChange={() => {}}
+            onlineOnly={false}
+            onOnlineOnlyChange={() => {}}
+            onOpenFilters={() => {}}
+            dateRange={{ from: undefined, to: undefined }}
+            onDateRangeChange={() => {}}
+            activeFilterCount={0}
+          />
+
+          {/* 실제 페이지 내용 */}
+          <main className="flex-1">{children}</main>
+        </div>
+      </body>
     </html>
   )
 }
